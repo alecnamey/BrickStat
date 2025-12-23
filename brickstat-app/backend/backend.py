1e32776df37c424de514b9f2fc00c36c4f3e5d73
Aldridge Alegre <almy.alegre@gmail.com>
2025-12-23 15:16:26 -0800

    Played around with set themes code

--- brickstat-app/backend/backend.py
+++ brickstat-app/backend/backend.py
@@ -13,6 +13,24 @@ if not API_KEY:
 
 BASE_URL = "https://rebrickable.com/api/v3/lego/sets/"
 
+THEME_URL = "https://rebrickable.com/api/v3/lego/themes/"
+
+
+## CACHE THEME LOOKUPS TO AVOID REPEATED CALLS AND GETTING BANNED FROM REBRICKABLE API
+theme_cache = {}
+def get_theme(theme_id, headers):
+    if theme_id in theme_cache:
+        return theme_cache[theme_id]
+    
+    theme_url = f"{THEME_URL}{theme_id}/"
+    theme_response = requests.get(theme_url, headers=headers, timeout=5)
+    if theme_response.status_code == 404:
+        print("error")
+        return
+    theme_cache[theme_id] = theme_response.json()
+    return theme_cache[theme_id]
+
+
 def get_set_by_number(set_num):
     headers = {
         "Authorization": f"key {API_KEY}"
@@ -20,22 +38,26 @@ def get_set_by_number(set_num):
 
     url = f"{BASE_URL}{set_num}/"
     response = requests.get(url, headers=headers, timeout=5)
-
     if response.status_code == 404:
         print("Invalid set number. Format should look like 75257-1.")
         return
-
     response.raise_for_status()
 
     s = response.json()
+    theme = get_theme(s['theme_id'], headers)
 
     print("\n=== LEGO SET FOUND ===")
     print(f"Name: {s['name']}")
     print(f"Set: {s['set_num']}")
+    print(f"Theme: {theme['name']}")
     print(f"Year: {s['year']}")
     print(f"Pieces: {s['num_parts']}")
     print(f"Image: {s['set_img_url']}")
     print("=====================\n")
+
+    # print(theme_cache)
+
+
 
 if __name__ == "__main__":
     while True:
